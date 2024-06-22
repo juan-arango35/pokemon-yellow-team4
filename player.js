06eb2f2b0b430c7473e1487db4a6898dbd37b29a
Lizethch <lizethchm4@gmail.com>
2024-06-21 19:33:26 -0500

    Se estan subiendo los archivos modificados. Se probo exitosamente battle.js, game.js, player.js e index.js. El pokemon.js debe ser completado.

--- player.js
+++ player.js
@@ -1,20 +1,36 @@
+
 class Player {
   constructor(name, species, pokeName, level) {
     // asignar name a un atributo con el mismo nombre
+    this.name = name;
     // crear un Pokemon con el resto de parametros y asignarlo al atributo pokemon
+    this.pokemon = new Pokemon(species, pokeName, level);
   }
 
   selectMove() {
-    // mostrar al usuario los movimientos dosponibles
-    // Volver a pedir si ingresa un movimiento invalido
-    // Asigna el movimiento con 'setCurrentMove'
-    // retornar 'true' en caso el usuario apreta Cancel
+    const moves = this.pokemon.moves; //Asigna la lista de movimientos del Pokémon del jugador a la constante moves.
+    let selectedMove; //Declara una variable para almacenar el movimiento seleccionado por el jugador.
+    while (!selectedMove) { //Inicia un bucle while que continuará hasta que selectedMove tenga un valor.
+      const userInput = prompt(
+        `${this.name}, Choose a move:\n${moves.map(move => move).join('\n')}\n`, moves[0]);//crea un mensaje que incluye el nombre del jugador ( en this.name) y muestra una lista de movimientos disponibles
+      if (userInput === null) return true; //Si el usuario cancela el prompt, el método selectMove retorna true y termina.
+      selectedMove = moves.find(move => move === userInput); // Busca en la lista de movimientos uno cuyo nombre coincida con el userInput y lo asigna a selectedMove.
+      if (!selectedMove) { //Si no se encuentra un movimiento válido (es undefined), muestra una alerta indicando que la opción es inválida.
+        alert("Invalid option");
+      }
+    }
+
+    this.pokemon.setCurrentMove(selectedMove); //Establece el movimiento actual del Pokémon del jugador al movimiento seleccionado.
   }
 }
 
-class Bot extends Player {
-  selectMove() {
-    // selecciona un movimiento de maner aleatoria
-    // los asigna con 'setCurrentMove'
+class Bot extends Player { //Se declara una nueva clase Bot que hereda de la clase Player
+  selectMove() { //Este método se encargará de seleccionar un movimiento para el Pokémon del bot.
+    const moves = this.pokemon.moves; //declara una constante moves, asigna el array de movimientos del Pokémon asociado con esta instancia de Bot.
+    const randomIndex = Math.floor(Math.random() * moves.length);//Calcula un índice aleatorio que se puede usar para acceder a un elemento en el array de movimientos.
+    const selectedMove = moves[randomIndex];// Toma un movimiento aleatorio del array moves y lo asigna a la variable selectedMove
+    this.pokemon.setCurrentMove(selectedMove);
+
+
   }
 }
--- pokemon.js
+++ pokemon.js
@@ -1,41 +1,98 @@
 class Pokemon {
   constructor(species, name, level) {
-    // Inicializar atributos usando los parámetros
-    // Inicializar atributos usando la información del Pokedex
-    // Inicializar atributos según otras indicaciones
-    // this.experiencePoints = ;
-    // this.individualValues = ;
-    // this.effortValues = ;
-  }
+    // 1)Inicializar atributos usando los parámetros
+    this.species = species;
+    this.name = name;
+    this.level = level;
+    // 2)Inicializar atributos usando la información del Pokedex
+    const pokeData = Pokemons.find(pokemon => pokemon.species === species);
+    this.type = pokeData.type;
+    this.baseStats = pokeData.baseStats;
+    this.moves = pokeData.moves;
+    // 3)Inicializar atributos según otras indicaciones
+    // 4)this.experiencePoints = ;
+    this.experiencePoints = ExperienceCurves[pokeData.growthRate](level);
+    this.growthRate = pokeData.growthRate,
+      // 5)this.individualValues = ;
+      this.individualValues = {
+        hp: Math.floor(Math.random() * 32),
+        attack: Math.floor(Math.random() * 32),
+        defense: Math.floor(Math.random() * 32),
+        specialAttack: Math.floor(Math.random() * 32),
+        specialDefense: Math.floor(Math.random() * 32),
+        speed: Math.floor(Math.random() * 32),
+      };
+    // 6)this.effortValues = ;
+    this.effortValues = {
+      hp: 0,
+      attack: 0,
+      defense: 0,
+      specialAttack: 0,
+      specialDefense: 0,
+      speed: 0,
+    };
 
+    if (pokeData.effortPoints) {
+      const effortType = pokeData.effortPoints.type;
+      const effortAmount = pokeData.effortPoints.amount;
+      this.effortValues[effortType] = effortAmount;
+    }
+  }
   get stats() {
-    const stats = {};
+    const stats = {
+      hp: this.calculateStat("hp"),
+      attack: this.calculateStat("attack"),
+      defense: this.calculateStat("defense"),
+      specialAttack: this.calculateStat("specialAttack"),
+      specialDefense: this.calculateStat("specialDefense"),
+      speed: this.calculateStat("speed"),
+    };
 
     // calcular las estadisticas actuales del Pokémon
 
     return stats;
   }
 
+  calculateStat(statName) {
+    const baseStat = this.baseStats[statName];
+    const iv = this.individualValues[statName];
+    const ev = this.effortValues[statName];
+    const level = this.level;
+
+    // Fórmula estándar para calcular las estadísticas de un Pokémon
+    const stat = Math.floor(((2 * baseStat + iv + Math.floor(ev / 4)) * level) / 100 + 5);
+
+    return stat;
+  }
+
   expForLevel(n) {
     // obtener la función de crecimiento del pokedex
     // retornar el resultado de llamar a la función pasando `n`
+    const growthRatePokemon = pokedex[this.growthRate];
+    return Math.ceil(growthRatePokemon(n));
   }
 
   prepareForBattle() {
     // asignar al atributo currentHp la estadistica HP del Pokemon
     // resetear el atributo currentMove a null
+    this.currentHp = this.baseStats.hp,
+      this.currentMove = null
+
   }
 
-  receiveDamage(damage) {
+  receiveDamage(damage) { // El método receiveDamage se usa para reducir los puntos de vida .
     // reducir currentHp en la cantidad de damage. No debe quedar menor a 0.
+    this.currentHp = Math.max(0, this.currentHp - damage);//Esto actualiza los puntos de vida actuales del objeto después de recibir el daño.
   }
 
   setCurrentMove(move) {
     // buscar el move (string) en el pokedex y asignarlo al atributo currentMove
+    this.currentMove = Moves.find(pokemonMove => pokemonMove.name === move);
   }
 
   isFainted() {
     // retornar si currentHp es 0 o no
+    return this.currentHp === 0;
   }
 
   attack(target) {
@@ -67,10 +124,16 @@ class Pokemon {
     // determinar si se usara el stat attack o specialAttack del atacante
     // determinar si se usara el stat defense o specialDefense del defensor
     // retornar el rsultado de la formula de daño
+    const isSpecial = specialMoveTypes.includes(this.currentMove.type);
+    const attackStat = isSpecial ? this.stats.specialAttack : this.stats.attack;
+    const defenseStat = isSpecial ? target.stats.specialDefense : target.stats.defense;
+
+    return Math.floor(((2 * this.level / 5 + 2) * this.currentMove.power * attackStat / defenseStat / 50) + 2);
   }
 
   calculateEffectiveness(target) {
     // caluclar el multiplicador de efectividad tomando el tipo del currentMove y el tipo de pokemon del oponente
+    return
   }
 
   processVictory(target) {
